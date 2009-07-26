8811d99498351257625038f0a059a6323c433ee5
ejucovy <ejucovy@e0e46c49-be69-4f5a-ad62-21024a331aea>
2009-07-26 00:32:31 +0000

    add latest-version changes to long_Description
    git-svn-id: https://my-svn.assembla.com/svn/arbez/sven/trunk@311 e0e46c49-be69-4f5a-ad62-21024a331aea

--- setup.py
+++ setup.py
@@ -3,10 +3,15 @@ import sys, os
 
 version = '0.2'
 
+long_description = open('README.txt').read()
+new_in_this_version = open('changes/changes.txt').read()
+
+long_description = "%s\n\nNew in version %s:\n\n%s" % (long_description,version,new_in_this_version)
+
 setup(name='sven',
       version=version,
       description="sven is a library to help you put content in SVN",
-      long_description=open('README.txt').read(),
+      long_description=long_description,
       classifiers=[],
       keywords='',
       author='Ethan Jucovy & Jeff Hammel',
